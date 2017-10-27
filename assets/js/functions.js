917dd28329ef073b4fb228a3bdb7ec87548247d5
Diego Neves <diego@diegoneves.eti.br>
2017-10-27 15:58:29 -0200

    Closes PrefeituraTO/e-pss#8

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,14 +7,24 @@ function verificarCPF(c){
     var v = false;
     var cpf = s;
 
-    if ( (cpf == "00000000000") || (cpf == "11111111111") || (cpf == "22222222222") || (cpf == "33333333333") 
-        || (cpf == "44444444444") || (cpf == "55555555555") || (cpf == "66666666666")
-        || (cpf == "77777777777") || (cpf == "88888888888") || (cpf == "99999999999")){
+    if (
+        (cpf == "00000000000") ||
+        (cpf == "11111111111") ||
+        (cpf == "22222222222") ||
+        (cpf == "33333333333") ||
+        (cpf == "44444444444") ||
+        (cpf == "55555555555") ||
+        (cpf == "66666666666") ||
+        (cpf == "77777777777") ||
+        (cpf == "88888888888") ||
+        (cpf == "99999999999")
+    ){
         alert("CPF Inválido")
         v = true;
         document.getElementById('cpf').value=null;
         return false; 
-    }  
+    }
+
     for (i = 0; i < 9; i++){
         d1 += c.charAt(i)*(10-i);
     }
@@ -25,6 +35,7 @@ function verificarCPF(c){
         alert("CPF Inválido")
         v = true;
         document.getElementById('cpf').value=null;
+        document.getElementById('CPF').value=null;
         return false;   
     }
  
@@ -38,13 +49,13 @@ function verificarCPF(c){
         alert("CPF Inválido")
         v = true;
         document.getElementById('cpf').value=null;
+        document.getElementById('CPF').value=null;
         return false;
         
     }
     if (!v) {
         
     }
-
 }
 
 function dataConta(c){
@@ -67,3 +78,113 @@ function formatacpf(c){
         c.value += '-'; 
     }
 }
+
+function validaForm(){
+    cont=0
+    if(document.getElementById("nome").value.length<5){
+        document.getElementById("nome").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("nome").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("mae").value.length<5){
+        document.getElementById("mae").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("mae").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("cpf").value.length<11){
+        document.getElementById("cpf").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("cpf").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("rg").value.length<7){
+        document.getElementById("rg").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("rg").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("datanasc").value.length<9){
+        document.getElementById("datanasc").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("datanasc").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("sexo").value==""){
+        document.getElementById("sexo").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("sexo").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("rua").value.length<2){
+        document.getElementById("rua").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("rua").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("numero").value.length<1){
+        document.getElementById("numero").style="border: solid 1px #ff0000"
+    }else{
+        document.getElementById("numero").style="border: solid 1px #dddddd"
+        cont++	
+    }
+
+    if(document.getElementById("bairro").value.length<3){
+        document.getElementById("bairro").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("bairro").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("cidade").value.length<7){
+        document.getElementById("cidade").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("cidade").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("uf").value==""){
+        document.getElementById("uf").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("uf").style="border: solid 1px #dddddd"
+    }
+
+    if(document.getElementById("email").value.length<7){
+        document.getElementById("email").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("email").style="border: solid 1px #dddddd"
+    }
+
+    if((document.getElementById("senha").value.length<6) || (document.getElementById("senha").value != document.getElementById("confirmar").value)) {
+        document.getElementById("senha").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("senha").style="border: solid 1px #dddddd"
+    }
+
+    if((document.getElementById("confirmar").value.length<6) || (document.getElementById("senha").value != document.getElementById("confirmar").value)) {
+        document.getElementById("confirmar").style="border: solid 1px #ff0000"
+        cont++	
+    }else{
+        document.getElementById("confirmar").style="border: solid 1px #dddddd"
+    }
+
+   if(cont > 0){
+   	alert("Campos obrigatórios não foram preenchidos ou estão preenchidos de forma incorreta")
+        return false
+   }else{
+        return true
+   }
+}
+
+
